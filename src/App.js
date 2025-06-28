d35d8b602d5acdb0a4459f018b5ca4ec6cd453e1
KETOSE\user <amumalbot4@gmail.com>
2025-06-28 10:54:01 +0900

    설문조사 페이지, 가이드 페이지

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Main from "./page/Main";
+import Guide from "./page/Guide";
+import MoodSurvey from "./page/MoodSurvey";
 
 function App() {
   return (
     <div className="app-container">
       <Router>
         <Header />
-          <Routes>
-            <Route path="/" element={<Main />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/survey" element={<MoodSurvey />} />
+          <Route path="/input" element={<div>직접 입력 페이지</div>} />
+          <Route path="/contents" element={<div>추천 콘텐츠 페이지</div>} />
+        </Routes>
         <Footer />
       </Router>
     </div>
