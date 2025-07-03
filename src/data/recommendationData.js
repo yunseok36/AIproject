fb3ea0aa008233cf86fb11734dc34ce24acae52b
yunseok36 <kys010306@gmail.com>
2025-07-04 01:26:56 +0900

    포스터 추가 완료

--- src/data/recommendationData.js
+++ src/data/recommendationData.js
@@ -4,10 +4,100 @@ import twiceImage from '../image/CheerUp.png';
 import iuIamge from '../image/GoodDay.png';
 import brunoImage from '../image/Uptown Funk.png';
 import zicoImage from '../image/Anything.png';
+import anImage from '../image/2002.png';
 import newImage from '../image/Super Shy.png';
 import doaImage from '../image/DSN.png';
 import maroonImage from '../image/Sugar.png';
 import bigbangImage from '../image/FantasticBaby.png';
+import bolImage from '../image/Travel.png';
+import backImage from '../image/Square.png';
+import psyImage from '../image/ThatThat.png';
+import blockImage from '../image/HER.png';
+import busImage from '../image/Ending.png';
+import kimImage from '../image/Amor.png';
+import djImage from '../image/Djdoc.png';
+import btsImage from '../image/Dynamite.png';
+import newImage from '../image/Super Shy.png';
+import iveImage from '../image/AfterLike.png';
+import acmuImage from '../image/GiveLove.png';
+import iu2Image from '../image/Palette.png';
+import crushImage from '../image/Beautiful.png';
+import melomImage from '../image/Present.png';
+import jeokImage from '../image/Star.png';
+import jangImage from '../image/Flower.png';
+import janimage from '../image/JanNavi.png'
+import sonImage from '../image/Run.png';
+import jungImage from '../image/Sky.png';
+import hyunImage from '../image/Bambi.png';
+import iu3Image from '../image/NightLatter.png';
+import taeImage from '../image/I.png';
+import hiImage from '../image/Hansum.png';
+import parkImage from '../image/Flower.png';
+import seoungImage from '../image/You.png';
+import polImage from '../image/Everyday.png';
+import jungImage from '../image/Hug.png';
+import akmu2Image from '../image/How.png';
+import naImage from '../image/Say.png';
+import heImage from '../image/Rain.png';
+import bpImage from '../image/DduduDdudu.png';
+import gidleImage from '../image/Tomboy.png';
+import itzyImage from '../image/DallaDalla.png';
+import billyImage from '../image/BadGuy.png';
+import eminemImage from '../image/RapGod.png';
+import mamaImage from '../image/Hip.png';
+import epicImage from '../image/BornHater.png';
+import jessiImage from '../image/NunuNana.png';
+import ybImage from '../image/Nabi.png';
+import kimkyungImage from '../image/Father.png';
+import upImage from '../image/Up.png';
+import paddingtonImage from '../image/Paddington.png';
+import schoolImage from '../image/SchoolOfRock.png';
+import artistImage from '../image/Artist.png';
+import augustImage from '../image/AugustRush.png';
+import beginAgainImage from '../image/BeginAgain.png';
+import soulImage from '../image/Soul.png';
+import ratatouilleImage from '../image/Ratatouille.png';
+import wonderImage from '../image/Wonder.png';
+import goodMorningImage from '../image/GoodMorning.png';
+import aboutTimeImage from '../image/AboutTime.png';
+import laLaLandImage from '../image/LalaLand.png';
+import littleImage from '../image/Littleforest.png';
+import amelieImage from '../image/Amerie.png';
+import flipImage from '../image/Flipped.png';
+import naImage from '../image/Brother.png';
+import mostNormalImage from '../image/Love.png';
+import singImage from '../image/Sing.png';
+import paddington2Image from '../image/Paddington2.png';
+import chefImage from '../image/Chef.png';
+import inceptionImage from '../image/Inception.png';
+import tenetImage from '../image/Tenet.png';
+import knivesImage from '../image/KnivesOut.png';
+import contactImage from '../image/Contact.png';
+import interstellarImage from '../image/Interstellar.png';
+import spiderImage from '../image/Spiderman.png';
+import socialImage from '../image/Social.png';
+import cameraImage from '../image/StopCamera.png';
+import busanImage from '../image/Pusan.png';
+import exitImage from '../image/Exit.png';
+import roomImage from '../image/Room.png';
+import manchesterImage from '../image/Manchester.png';
+import farewellImage from '../image/Farewell.png';
+import marriageImage from '../image/Marry.png';
+import blueImage from '../image/Blue.png';
+import burningImage from '../image/Burning.png';
+import kindnessImage from '../image/Kind.png';
+import ourImage from '../image/We.png';
+import nationalImage from '../image/Nation.png';
+import searchImage from '../image/Search.png';
+import jokerImage from '../image/Joker.png';
+import getOutImage from '../image/Getout.png';
+import prisonersImage from '../image/Prison.png';
+import hereditaryImage from '../image/Hereditary.png';
+import midsommarImage from '../image/Mid.png';
+import whiplashImage from '../image/Whiplash.png';
+import motherImage from '../image/Mother.png';
+import sawImage from '../image/Gokseong.png';
+import oldboyImage from '../image/Oldboy.png';
 
 
 export const songRecommendations = {
@@ -18,120 +108,120 @@ export const songRecommendations = {
     { name: "아이유", title: "좋은 날", year: 2010, image: iuIamge},
     { name: "브루노 마스", title: "Uptown Funk", year: null, image: brunoImage},
     { name: "지코", title: "아무노래", year: 2020, image: zicoImage},
-    { name: "뉴진스", title: "Super Shy", year: 2023, image:newImage},
+    { name: "안나 마리", title: "2002", year: 2018, image:anImage},
     { name: "두아 리파", title: "Don't Start Now", year: 2019, image: doaImage},
     { name: "마룬 5", title: "Sugar", year: 2015, image: maroonImage},
     { name: "빅뱅", title: "FANTASTIC BABY", year: 2012, image: bigbangImage},
   ],
   "긍정적": [
-    { name: "볼빨간 사춘기", title: "여행", year: 2018 },
-    { name: "백예린", title: "Square", year: 2017 },
-    { name: "싸이", title: "That That", year: 2022 },
-    { name: "블락비", title: "HER", year: 2014 },
-    { name: "버스커 버스커", title: "벚꽃 엔딩", year: 2012 },
-    { name: "김연자", title: "아모르 파티", year: 2013 },
-    { name: "DJ DOC", title: "나 이런 사람이야", year: 2010 },
-    { name: "BTS", title: "Dynamite", year: null },
-    { name: "뉴진스", title: "Super Shy", year: 2023 },
-    { name: "아이브", title: "After LIKE", year: 2022 },
+    { name: "볼빨간 사춘기", title: "여행", year: 2018, image: bolImage },
+    { name: "백예린", title: "Square", year: 2017, image: backImage },
+    { name: "싸이", title: "That That", year: 2022, image: psyImage },
+    { name: "블락비", title: "HER", year: 2014, image: blockImage },
+    { name: "버스커 버스커", title: "벚꽃 엔딩", year: 2012, image: busImage },
+    { name: "김연자", title: "아모르 파티", year: 2013, image: kimImage },
+    { name: "DJ DOC", title: "나 이런 사람이야", year: 2010, image: djImage },
+    { name: "BTS", title: "Dynamite", year: null, image: btsImage },
+    { name: "뉴진스", title: "Super Shy", year: 2023, image: newImage },
+    { name: "아이브", title: "After LIKE", year: 2022, image: iveImage },
   ],
   "보통": [
-    { name: "악동뮤지션", title: "Give Love", year: 2014 },
-    { name: "아이유", title: "팔레트 (Feat. G-DRAGON)", year: 2017 },
-    { name: "Crush", title: "Beautiful", year: 2016 },
-    { name: "멜로망스", title: "선물", year: 2017 },
-    { name: "적재", title: "별 보러 가자", year: 2018 },
-    { name: "장범준", title: "흔들리는 꽃들 속에서 네 샴푸향이 느껴진 거야", year: 2019 },
-    { name: "잔나비", title: "주저하는 연인들을 위해", year: 2019 },
-    { name: "선우정아", title: "도망가자", year: 2020 },
-    { name: "정은지", title: "하늘바라기 (Feat. 하림)", year: 2016 },
-    { name: "백현", title: "Bambi", year: 2021 },
+    { name: "악동뮤지션", title: "Give Love", year: 2014, image: acmuImage },
+    { name: "아이유", title: "팔레트 (Feat. G-DRAGON)", year: 2017, image: iu2Image },
+    { name: "Crush", title: "Beautiful", year: 2016, image: crushImage },
+    { name: "멜로망스", title: "선물", year: 2017, image: melomImage },
+    { name: "적재", title: "별 보러 가자", year: 2018, image: jeokImage },
+    { name: "장범준", title: "흔들리는 꽃들 속에서 네 샴푸향이 느껴진 거야", year: 2019, image: jangImage },
+    { name: "잔나비", title: "주저하는 연인들을 위해", year: 2019, image: janimage },
+    { name: "선우정아", title: "도망가자", year: 2020, image: sonImage },
+    { name: "정은지", title: "하늘바라기 (Feat. 하림)", year: 2016, image: jungImage },
+    { name: "백현", title: "Bambi", year: 2021, image: hyunImage },
   ],
   "부정적": [
-    { name: "IU", title: "밤편지", year: 2017 },
-    { name: "태연", title: "I", year: 2015 },
-    { name: "이하이", title: "한숨", year: 2016 },
-    { name: "박효신", title: "야생화", year: 2014 },
-    { name: "정승환", title: "너였다면", year: 2016 },
-    { name: "폴킴", title: "모든 날, 모든 순간", year: 2018 },
-    { name: "정준일", title: "안아줘", year: 2011 },
-    { name: "악동뮤지션", title: "어떻게 이별까지 사랑하겠어, 널 사랑하는 거지", year: 2019 },
-    { name: "김나영", title: "솔직하게 말해서 나", year: 2019 },
-    { name: "헤이즈", title: "비도 오고 그래서", year: 2017 },
+    { name: "IU", title: "밤편지", year: 2017, image: iu3Image },
+    { name: "태연", title: "I", year: 2015, image: taeImage },
+    { name: "이하이", title: "한숨", year: 2016, image: hiImage },
+    { name: "박효신", title: "야생화", year: 2014, image: parkImage },
+    { name: "정승환", title: "너였다면", year: 2016, image: seoungImage },
+    { name: "폴킴", title: "모든 날, 모든 순간", year: 2018, image: polImage },
+    { name: "정준일", title: "안아줘", year: 2011, image: jungImage },
+    { name: "악동뮤지션", title: "어떻게 이별까지 사랑하겠어, 널 사랑하는 거지", year: 2019, image: akmu2Image },
+    { name: "김나영", title: "솔직하게 말해서 나", year: 2019, image: naimage },
+    { name: "헤이즈", title: "비도 오고 그래서", year: 2017, image: heImage },
   ],
   "아주 부정적": [
-    { name: "블랙핑크", title: "뚜루뚜루", year: 2018 },
-    { name: "여자아이들", title: "TOMBOY", year: 2022 },
-    { name: "잇지", title: "달라달라", year: 2019 },
-    { name: "빌리 아일리시", title: "Bad Guy", year: 2019 },
-    { name: "에미넴", title: "Rap God", year: 2013 },
-    { name: "마마무", title: "HIP", year: 2019 },
-    { name: "에픽하이", title: "Born Hater", year: 2014 },
-    { name: "Jessi", title: "눈누난나", year: 2020 },
-    { name: "YB", title: "나는 나비", year: 2011 },
-    { name: "김경호", title: "아버지", year: 2011 },
+    { name: "블랙핑크", title: "뚜루뚜루", year: 2018 , image: bpImage },
+    { name: "여자아이들", title: "TOMBOY", year: 2022, image: gidleImage },
+    { name: "잇지", title: "달라달라", year: 2019, image: itzyImage },
+    { name: "빌리 아일리시", title: "Bad Guy", year: 2019, image: billyImage },
+    { name: "에미넴", title: "Rap God", year: 2013, image: eminemImage },
+    { name: "마마무", title: "HIP", year: 2019, image: mamaImage },
+    { name: "에픽하이", title: "Born Hater", year: 2014, image: epicImage },
+    { name: "Jessi", title: "눈누난나", year: 2020, image: jessiImage },
+    { name: "YB", title: "나는 나비", year: 2011, image: ybImage },
+    { name: "김경호", title: "아버지", year: 2011, image: kimkyungImage },
   ],
 };
 
 export const movieRecommendations = {
   "아주 긍정적": [
-    { title: "업", year: 2009, director: null },
-    { title: "패딩턴", year: 2014, director: null },
-    { title: "스쿨 오브 락", year: 2003, director: null },
-    { title: "아티스트", year: 2011, director: null },
-    { title: "어거스트 러쉬", year: 2007, director: null },
-    { title: "비긴 어게인", year: 2013, director: null },
-    { title: "소울", year: 2020, director: null },
-    { title: "라따뚜이", year: 2007, director: null },
-    { title: "원더", year: 2017, director: null },
-    { title: "굿모닝 에브리원", year: 2010, director: null },
+    { title: "업", year: 2009, director: null, image: upImage },
+    { title: "패딩턴", year: 2014, director: null, image: paddingtonImage },
+    { title: "스쿨 오브 락", year: 2003, director: null, image: schoolImage },
+    { title: "아티스트", year: 2011, director: null, image: artistImage },
+    { title: "어거스트 러쉬", year: 2007, director: null, image: augustImage },
+    { title: "비긴 어게인", year: 2013, director: null, image: beginAgainImage },
+    { title: "소울", year: 2020, director: null, image: soulImage },
+    { title: "라따뚜이", year: 2007, director: null, image: ratatouilleImage },
+    { title: "원더", year: 2017, director: null, image: wonderImage },
+    { title: "굿모닝 에브리원", year: 2010, director: null, image: goodMorningImage },
   ],
   "긍정적": [
-    { title: "어바웃 타임", year: 2013, director: null },
-    { title: "라라랜드", year: 2016, director: null },
-    { title: "리틀 포레스트", year: 2018, director: null },
-    { title: "아멜리에", year: 2001, director: null },
-    { title: "플립", year: 2010, director: null },
-    { title: "나의 특별한 형제", year: 2019, director: null },
-    { title: "가장 보통의 연애", year: 2019, director: null },
-    { title: "싱 스트리트", year: 2016, director: null },
-    { title: "패딩턴 2", year: 2017, director: null },
-    { title: "셰프", year: 2014, director: null },
+    { title: "어바웃 타임", year: 2013, director: null, image: aboutTimeImage },
+    { title: "라라랜드", year: 2016, director: null, image: laLaLandImage },
+    { title: "리틀 포레스트", year: 2018, director: null, image: littleImage },
+    { title: "아멜리에", year: 2001, director: null, image: amelieImage },
+    { title: "플립", year: 2010, director: null, image: flipImage },
+    { title: "나의 특별한 형제", year: 2019, director: null, image: naImage },
+    { title: "가장 보통의 연애", year: 2019, director: null, image: mostNormalImage },
+    { title: "싱 스트리트", year: 2016, director: null, image: singImage },
+    { title: "패딩턴 2", year: 2017, director: null, image: paddington2Image },
+    { title: "셰프", year: 2014, director: null, image: chefImage },
   ],
   "보통": [
-    { title: "인셉션", year: 2010, director: null },
-    { title: "테넷", year: 2020, director: null },
-    { title: "나이브스 아웃", year: 2019, director: null },
-    { title: "컨택트", year: 2016, director: null },
-    { title: "인터스텔라", year: 2014, director: null },
-    { title: "스파이더맨: 뉴 유니버스", year: 2018, director: null },
-    { title: "소셜 네트워크", year: 2010, director: null },
-    { title: "카메라를 멈추면 안 돼!", year: 2017, director: null },
-    { title: "부산행", year: 2016, director: null },
-    { title: "엑시트", year: 2019, director: null },
+    { title: "인셉션", year: 2010, director: null, image: inceptionImage },
+    { title: "테넷", year: 2020, director: null, image: tenetImage },
+    { title: "나이브스 아웃", year: 2019, director: null, image: knivesImage },
+    { title: "컨택트", year: 2016, director: null, image: contactImage },
+    { title: "인터스텔라", year: 2014, director: null, image: interstellarImage },
+    { title: "스파이더맨: 뉴 유니버스", year: 2018, director: null, image: spiderImage },
+    { title: "소셜 네트워크", year: 2010, director: null, image: socialImage },
+    { title: "카메라를 멈추면 안 돼!", year: 2017, director: null, image: cameraImage },
+    { title: "부산행", year: 2016, director: null, image: busanImage },
+    { title: "엑시트", year: 2019, director: null, image: exitImage },
   ],
   "부정적": [
-    { title: "룸", year: 2015, director: null },
-    { title: "맨체스터 바이 더 씨", year: 2016, director: null },
-    { title: "페어웰", year: 2019, director: null },
-    { title: "결혼 이야기", year: 2019, director: null },
-    { title: "블루 발렌타인", year: 2010, director: null },
-    { title: "버닝", year: 2018, director: null },
-    { title: "친절한 금자씨", year: 2005, director: null },
-    { title: "우리들", year: 2016, director: null },
-    { title: "국가부도의 날", year: 2018, director: null },
-    { title: "서치", year: 2018, director: null },
+    { title: "룸", year: 2015, director: null, image: roomImage },
+    { title: "맨체스터 바이 더 씨", year: 2016, director: null, image: manchesterImage },
+    { title: "페어웰", year: 2019, director: null, image: farewellImage },
+    { title: "결혼 이야기", year: 2019, director: null, image: marriageImage },
+    { title: "블루 발렌타인", year: 2010, director: null, image: blueImage },
+    { title: "버닝", year: 2018, director: null, image: burningImage },
+    { title: "친절한 금자씨", year: 2005, director: null, image: kindnessImage },
+    { title: "우리들", year: 2016, director: null, image: ourImage },
+    { title: "국가부도의 날", year: 2018, director: null, image: nationalImage },
+    { title: "서치", year: 2018, director: null, image: searchImage },
   ],
   "아주 부정적": [
-    { title: "조커", year: 2019, director: null },
-    { title: "겟 아웃", year: 2017, director: null },
-    { title: "프리즈너스", year: 2013, director: null },
-    { title: "유전", year: 2018, director: null },
-    { title: "미드소마", year: 2019, director: null },
-    { title: "위플래쉬", year: 2014, director: null },
-    { title: "마더", year: 2009, director: null },
-    { title: "악마를 보았다", year: 2010, director: null },
-    { title: "곡성", year: 2016, director: null },
-    { title: "올드보이", year: 2003, director: null },
+    { title: "조커", year: 2019, director: null, image: jokerImage },
+    { title: "겟 아웃", year: 2017, director: null, image: getOutImage },
+    { title: "프리즈너스", year: 2013, director: null, image: prisonersImage },
+    { title: "유전", year: 2018, director: null, image: hereditaryImage },
+    { title: "미드소마", year: 2019, director: null, image: midsommarImage },
+    { title: "위플래쉬", year: 2014, director: null, image: whiplashImage },
+    { title: "마더", year: 2009, director: null, image: motherImage },
+    { title: "악마를 보았다", year: 2010, director: null, image: sawImage },
+    { title: "곡성", year: 2016, director: null, image: sawImage },
+    { title: "올드보이", year: 2003, director: null, image: oldboyImage },
   ],
 };
